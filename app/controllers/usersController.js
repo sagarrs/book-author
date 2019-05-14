6d190b86505641cf0afb35ce1531bf2a29bea772
sagar <sagarrs293@gmail.com>
2019-05-14 12:36:59 +0530

    user auth version 1

--- app/controllers/usersController.js
+++ app/controllers/usersController.js
@@ -2,9 +2,9 @@ const express = require("express")
 const router = express.Router()
 
 const {User} = require("../models/user")
+const {authenticateUser} = require("../middlewares/authentication")
 
-
-router.get("/", (req, res) => {
+router.get("/", authenticateUser, (req, res) => {
     User.find()
         .then((user) => {
             res.status("200").send(user)
@@ -14,17 +14,56 @@ router.get("/", (req, res) => {
         })
 })
 
-router.get("/:id", (req, res) => {
-    const id = req.params.id
 
-    User.find({_id: id})
+router.post("/register", authenticateUser, (req, res) => {
+    const body = req.body
+
+    const user = new User(body)
+
+    user.save()
         .then((user) => {
-            res.status("200").send(user)
+            res.send(user)
         })
         .catch((err) => {
-            res.status("404").send(err)
+            res.send(err)
         })
 })
+
+router.post("/login", (req, res) => {
+    const body = req.body
+
+    User.findByCredentials(body.email, body.password)
+        .then((user) => {
+            return user.generateToken()  
+        })
+        .then((token) => {
+            // res.send({token})
+            res.setHeader("x-auth", token).send({})
+        })
+        .catch((err) => {
+            res.send(err)
+        })
+})
+
+router.get("/account", authenticateUser, (req, res) => {
+    const {user} = req
+    // res.send is doen here and not in middleware is coz it shouldnt be done, and there might be 
+    // some ops that needs to be done later, therefore we should send from controller
+    res.send(user)
+})
+
+router.delete("/logout", authenticateUser, (req, res) => {
+    const {user, token} = req
+
+    User.findByIdAndUpdate(user._id, {$pull: {tokens: {token: token}}})
+        .then(() => {
+            res.send({notice:"loggedout successfully"})
+        })
+        .catch((err) => {
+            res.send(err)
+        })
+})
+
 module.exports = {
     userRouter: router
 }
